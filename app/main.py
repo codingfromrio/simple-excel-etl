cb9fc49bbabcaa1f4e023073ce7b52542dc3dbe9
Philip Martins <philip.martt@outlook.com>
2024-05-14 00:36:15 -0300

    feat: updated handler function

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from pipe.extract import extract_from_excel
 from pipe.transform import concat_dataframes
 from pipe.load import write_df_to_excel
 
-if __name__ == '__main__':
+def main():
     data_frame_list = extract_from_excel("data/input")
     drata_frame = concat_dataframes(data_frame_list)
     write_df_to_excel(drata_frame, "data/output/output.xlsx")
+
+if __name__ == '__main__':
+    main()
